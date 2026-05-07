2eb1fbacc606adc93096ca45cc34ba5dedfc3664
hazemfatnassi <fatnassi.hazem2005@gmail.com>
2026-05-07 16:38:52 +0100

    RD-26 feat: improve child analytics system, monitoring, grpc communication and dynamic dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
+import { Metadata } from 'next';
 import '../styles/globals.css';
 import { SidebarProvider } from '@/contexts/SidebarContext';
+
+export const metadata: Metadata = {
+  title: 'Readdly | Administration',
+  description: 'Portail d\'administration de la plateforme Readdly',
+  icons: {
+    icon: '/images/logo-readdly.png',
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
